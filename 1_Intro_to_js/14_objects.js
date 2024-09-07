48adf5360286a95d7e21fdb3674dd8e40925c509
Ayush Dutta <yorksdev@gmail.com>
2024-09-07 15:49:53 +0800

    js basics

---
+++ 1_Intro_to_js/14_objects.js
@@ -0,0 +1,36 @@
+/*
+    1. In object keys are unique like dict in python
+*/
+
+let student1 = {
+    name : "Ayush",
+    age : 24,
+    mark : 50
+}
+
+// One way of doing it
+for(let item of Object.keys(student1)){
+    console.log(`${item} : ${student1[item]}`)
+}
+
+// For each works for arrays
+Object.keys(student1).forEach((keys) => {
+    console.log(keys, student1[keys])
+})
+
+// Change values
+student1.name = "Lugen"
+student1["name"] = "Lugen1"   // Just like python
+
+console.log(student1)
+
+// add new property or key
+student1["house"] = "Monlaque"
+student1.house2 = "Krishnagar"
+
+console.log(student1)
+
+delete student1["mark"]
+delete student1.mark    // both works
+
+console.log(student1)
---
+++ 1_Intro_to_js/15_object_problems.js
@@ -0,0 +1,10 @@
+// Like python we will do grouping of arrays
+
+array1 =  ['ayush', 'lugen', 'billal', 'joshua', 'jared', 12]
+
+// group based on their length
+
+for(let item of array1){
+    item = String(item)
+    item.split
+}
